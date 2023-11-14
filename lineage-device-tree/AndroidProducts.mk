15f8892ee07e4b1dab1fdcfc3e6ec61f20918145
shazu <ejsh@gmail.com>
2023-11-14 06:59:48 +0000

    Add extras for missi_phoneext4_global-user 13 TP1A.220624.014 V14.0.4.0.TKEINXM release-keys
    Signed-off-by: shazu <ejsh@gmail.com>

---
+++ lineage-device-tree/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_fleur.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_fleur-user \
+    lineage_fleur-userdebug \
+    lineage_fleur-eng
